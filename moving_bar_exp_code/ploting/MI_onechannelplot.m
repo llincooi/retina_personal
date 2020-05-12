d84549d805a789f6d505add250c627a007c3e4f6
llincooi <33573023+llincooi@users.noreply.github.com>
2020-05-12 20:49:37 +0800

    0512p

--- moving_bar_exp_code/ploting/MI_onechannelplot.m
+++ moving_bar_exp_code/ploting/MI_onechannelplot.m
@@ -1,7 +1,7 @@
 %clear all;
 close all;
-for channelnumber=43
-%figure;
+for channelnumber=10:14
+figure;
 plot(time,Mutual_infos{channelnumber},'LineWidth',1.5); hold on; %,'color',cc(z,:));hold on
 plot(time,Mutual_shuffle_infos{channelnumber},'LineWidth',1.5); hold on; %,'color',cc(z,:));hold on
 xlabel('\deltat (ms)');ylabel('MI (bits/second)');
@@ -11,10 +11,7 @@ set(gca,'fontsize',12); hold on
 
 xlim([ -3000 3000])
 % ylim([0 inf])
-title(['channel ',num2str(channelnumber)]) 
-
-
-
+title(['channel ',num2str(channelnumber)])
 end
 
 
