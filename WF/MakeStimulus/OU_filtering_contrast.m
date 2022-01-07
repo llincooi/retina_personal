0feb851b4cde865e6f45ed455b9df618f2710c42
llincooi <33573023+llincooi@users.noreply.github.com>
2022-01-07 18:10:46 +0800

    MakeMovie

--- WF/MakeStimulus/OU_filtering_contrast.m
+++ WF/MakeStimulus/OU_filtering_contrast.m
@@ -4,9 +4,9 @@ close all
 path='\\ZebraNas\Public\Retina\WF_stimuli\211217\';
 
 Tot = 300;
-dt = 0.01; 
+dt = 0.01; %100Hz
 T = dt:dt:Tot;
-tau=0.5;
+tau = 0.5;
 D = 1; % dynamical range
 at = 60;
 a2=[];ey=[];ey1=[];
@@ -31,7 +31,7 @@ mean_set=[4 7 10 13];
 C_set=[0.1,0.15,0.2,0.3];
 
 figure(4);hold on;
-[t,ey,a2]=generate_stimulus(L,10,2,dt,24);  % (signal, mean intensity, times of std)
+[t,ey,a2] = generate_stimulus(L,10,2,dt,24);  % (signal, mean intensity, times of std)
 plot(t,ey,'linewidth',1);
 if saving
     save([path,'OU_tau=',num2str(tau*1000),'ms_cutoff=0_mean=10_amp=2.mat'],'ey','a2','t')
@@ -59,8 +59,10 @@ end
 %%
 figure
 for cutoff=[2,4]
+    
     [b,a]=butter(2,2*cutoff*dt,'low');
     Lf = filtfilt(b,a,L);
+    
     mean=10;
     amp=10*0.2;
     [t,ey,a2]=generate_stimulus(Lf,mean,amp,dt,24);
@@ -68,6 +70,7 @@ for cutoff=[2,4]
     if saving
         save([path,'OU_tau=',num2str(tau*1000),'ms_cutoff=',num2str(cutoff),'_mean=10_C=0.2.mat'],'ey','a2','t')
     end
+    
 end
 
 
--- WF/MakeStimulus/white_noise_generation.m
+++ WF/MakeStimulus/white_noise_generation.m
@@ -1,36 +1,20 @@
 % white noise stimulus
 clear
 close all
-savepath='\\ZebraNas\Public\Retina\WF_stimuli\211217';
-Tot=300;
-dt=0.05;
-x_rand=randn(1,Tot/dt);
+savepath = '\\ZebraNas\Public\Retina\WF_stimuli\211217';
+Tot = 300;
+dt = 0.05; % 20Hz
+x_rand = randn(1,Tot/dt);
 %% different mean different contrast 20211029
-mean_set=[4 7 10 13];
-C_set=[0.1,0.15,0.2,0.3];
+mean_set = [4 7 10 13];
+C_set = [0.1,0.15,0.2,0.3];
 for i=1:length(mean_set)
     figure(i);hold on
     for j=1:length(C_set)
         mean=mean_set(i);
         amp=mean*C_set(j);
-        [t,ey,a2]=generate_stimulus(x_rand,mean,amp,dt,24);
-        plot(t,ey)
-        save([savepath,'\WhiteNoise_mean=',num2str(mean),'_C=',num2str(C_set(j)),'.mat'],'t','ey','a2')
+        [t,ey,a2] = generate_stimulus(x_rand,mean,amp,dt,24);
+        plot(a2)
+%         save([savepath,'\WhiteNoise_mean=',num2str(mean),'_C=',num2str(C_set(j)),'.mat'],'t','ey','a2')
     end
 end
-
-%% 
-% amp_set=[0.5,1,1.5,2,3];
-% for i = 1%:length(amp_set)
-%     mean=10;
-%     amp=2 %amp_set(i);
-%     [t,inten,a2]=generate_stimulus(x_rand,mean,amp,dt);
-% %     save([savepath,'\WhiteNoise_mean=',num2str(mean),'_amp=',num2str(amp),'.mat'],'t','inten','a2')
-%     plot(t,inten,'linewidth',1);hold on
-% end
-
-% title('White noise stimulus')
-% xlabel('time (s)')
-% ylabel('light intensity (mW/m^2)')
-% set(gcf,'Position',[300,300,600,300])
-% xlim([100,104])
